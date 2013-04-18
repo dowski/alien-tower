981a46cf2d8dd323411501d5ce69d15b7c3ceefb
Christian Wyglendowski <christian@dowski.com>
2013-04-17 23:56:14 -0400

    A square you can move around with the keyboard
    In ~100 lines. This is my first try at anything of substance in PyGame.
    I don't really know what I'm doing yet, but it's pretty fun.

---
+++ game.py
@@ -0,0 +1,104 @@
+import pygame
+import pygame.locals
+
+clock = pygame.time.Clock()
+
+pygame.init()
+screen = pygame.display.set_mode((300,200))
+pygame.display.set_caption('Alien Tower')
+
+background = pygame.Surface(screen.get_size())
+background = background.convert()
+background.fill((240, 240, 240))
+
+class Player(object):
+    MAX_JUMP = 40
+    def __init__(self, pos_x, pos_y):
+        self.viz = pygame.Surface((20, 20))
+        self.viz = self.viz.convert()
+        self.viz.fill((0,0,0))
+        self.pos = self.viz.get_rect()
+        self.pos.x = pos_x
+        self.pos.y = pos_y
+        self.prev = self.pos.copy()
+        self.blank = self.viz.copy()
+        self.blank.fill((240, 240, 240))
+        self.moving = 0
+        self.jump_at = 0
+        self.jump_direction = 0
+        self.jumping = False
+
+    def draw_on(self, surface):
+        surface.blit(self.blank, self.prev)
+        surface.blit(self.viz, self.pos)
+
+    def move_left_on(self, surface):
+        self.pos.x -= 1
+        self.draw_on(surface)
+        self.prev = self.pos.copy()
+
+    def move_right_on(self, surface):
+        self.pos.x += 1
+        self.draw_on(surface)
+        self.prev = self.pos.copy()
+
+    def animate_jump_on(self, surface):
+        if self.jump_at < self.MAX_JUMP and self.jump_direction > 0:
+            self.jump_at += 1
+            self.pos.y -= 1
+        elif self.jump_at > 0 and self.jump_direction < 0:
+            self.jump_at -= 1
+            self.pos.y += 1
+
+        if self.jump_at == 0:
+            self.jumping = False
+            self.jump_direction = 0
+        elif self.jump_at == self.MAX_JUMP:
+            self.jump_direction = -1
+        self.draw_on(surface)
+        self.prev = self.pos.copy()
+
+    def jump(self):
+        if not self.jumping:
+            self.jumping = True
+            self.jump_direction = 1
+
+player = Player(140, 180)
+
+player.draw_on(background)
+
+screen.blit(background, (0,0))
+pygame.display.flip()
+
+
+mod = 0
+LEFT = -1
+RIGHT = 1
+JUMP = 2
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.locals.QUIT:
+            raise SystemExit(0)
+        elif event.type == pygame.locals.KEYDOWN:
+            if event.key == pygame.locals.K_LEFT:
+                player.moving = -1
+            elif event.key == pygame.locals.K_RIGHT:
+                player.moving = 1
+            elif event.key == pygame.locals.K_UP:
+                player.jump()
+        elif event.type == pygame.locals.KEYUP:
+            print "keyup"
+            player.moving = 0
+
+    if player.moving == LEFT:
+        player.move_left_on(background)
+    elif player.moving == RIGHT:
+        player.move_right_on(background)
+    if player.jumping:
+        player.animate_jump_on(background)
+
+    if player.moving or player.jumping:
+        screen.blit(background, (0,0))
+        pygame.display.flip()
+    clock.tick(60)
