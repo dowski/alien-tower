e0ed07baad4bd9f624fb62034bb877bdf9308f00
Christian Wyglendowski <christian@dowski.com>
2013-04-21 20:16:25 -0400

    Added a platform the player can interact with
    The player can jump on the platform, and falls if it steps off.
    
    Programmed this along with Curtis (9) and Eli (7). They were flanking me
    while sitting on the couch, and it was quite possibly the most fun I've
    ever had programming.
    
    They both were totally engaged in it, contributing ideas, laughing at
    the various glitches along the way (when the "guy" wouldn't fall, kept
    jumping, fell through the floor, etc) and overall being amazed at how
    much work it took to simply get our box-shaped character to do what we
    want on the screen.

--- game.py
+++ game.py
@@ -3,6 +3,8 @@ import pygame.locals
 
 clock = pygame.time.Clock()
 
+ON_THE_FLOOR = 180
+
 pygame.init()
 screen = pygame.display.set_mode((300,200))
 pygame.display.set_caption('Alien Tower')
@@ -15,15 +17,22 @@ base = background.copy()
 
 tower = pygame.Surface((20, 200))
 tower = tower.convert()
-tower.fill((255, 0, 0))
+tower.fill((0, 254, 0))
 towerpos = tower.get_rect()
 towerpos.centerx = base.get_rect().centerx
 
+platform = pygame.Surface((40, 10))
+platform = platform.convert()
+platform.fill((50,0,232))
+platformpos = pygame.Rect(100,130,40,10)
+
 background.blit(tower, towerpos)
+
 base.blit(tower, towerpos)
+base.blit(platform, platformpos)
 
 class Player(object):
-    MAX_JUMP = 40
+    MAX_JUMP = 80
     def __init__(self, pos_x, pos_y, base):
         self.viz = pygame.Surface((20, 20))
         self.viz = self.viz.convert()
@@ -34,7 +43,7 @@ class Player(object):
         self.prev = self.pos.copy()
         self.blank = base
         self.moving = 0
-        self.jump_at = 0
+        self.jump_start = self.pos.y
         self.jump_direction = 0
         self.jumping = False
 
@@ -46,25 +55,40 @@ class Player(object):
         self.pos.x -= 1
         self.draw_on(surface)
         self.prev = self.pos.copy()
+        if not self.on_platform and not self.jumping:
+            self.fall()
 
     def move_right_on(self, surface):
         self.pos.x += 1
         self.draw_on(surface)
         self.prev = self.pos.copy()
+        if not self.on_platform and not self.jumping:
+            self.fall()
+
+    def fall(self):
+        self.jumping = True
+        self.jump_start = ON_THE_FLOOR
+        self.jump_direction = -1
+
+    @property
+    def on_platform(self):
+        if self.pos.y == ON_THE_FLOOR:
+            return True
+        elif self.pos.y == 110 and (self.pos.x > 80 and self.pos.x < 140):
+            return True
+        return False
 
     def animate_jump_on(self, surface):
-        if self.jump_at < self.MAX_JUMP and self.jump_direction > 0:
-            self.jump_at += 1
+        if self.jump_start - self.MAX_JUMP < self.pos.y and self.jump_direction > 0:
             self.pos.y -= 1
-        elif self.jump_at > 0 and self.jump_direction < 0:
-            self.jump_at -= 1
+        elif not self.on_platform and self.jump_direction < 0:
             self.pos.y += 1
+        else:
+            self.jump_direction = -1
 
-        if self.jump_at == 0:
+        if self.on_platform:
             self.jumping = False
             self.jump_direction = 0
-        elif self.jump_at == self.MAX_JUMP:
-            self.jump_direction = -1
         self.draw_on(surface)
         self.prev = self.pos.copy()
 
@@ -72,6 +96,7 @@ class Player(object):
         if not self.jumping:
             self.jumping = True
             self.jump_direction = 1
+            self.jump_start = self.pos.y
 
 player = Player(140, 180, base)
 
